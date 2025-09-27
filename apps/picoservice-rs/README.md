4a8fb1239d5cd06c34725567c62d1f6e73862cae
Thunder <robert.shalders@hotmail.com>
2025-09-27 06:57:41 +0100

    Renamed service and implemented more features

---
+++ apps/picoservice-rs/README.md
@@ -0,0 +1,8 @@
+# Picoservice-rs
+
+Simple application - exposes four endpoints on port 3000
+
+- / :: Homepage "hello world"
+- /slow :: Response delayed by a second
+- /health :: Response of version, app name & status
+- /metrics :: Prometheus metrics endpoint
--- apps/picoservice-rs/src/main.rs
+++ apps/picoservice-rs/src/main.rs
@@ -1,5 +1,7 @@
-use axum::{routing::get, Router};
+use axum::{routing::get, Json, Router};
 use axum_prometheus::PrometheusMetricLayer;
+use serde_json::{json, Value};
+use std::time::Duration;
 use tracing_subscriber::FmtSubscriber;
 
 #[tokio::main]
@@ -11,11 +13,32 @@ async fn main() {
 
     let (prometheus_layer, metric_handle) = PrometheusMetricLayer::pair();
     let app = Router::new()
-        .route("/", get(|| async { "Hello world!" }))
+        .route("/", get(index_handler))
+        .route("/slow", get(slow_handler))
+        .route("/health", get(health_handler))
         .route("/metrics", get(|| async move { metric_handle.render() }))
         .layer(prometheus_layer);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn index_handler() -> &'static str {
+    "Greetings!"
+}
+
+async fn slow_handler() -> &'static str {
+    tokio::time::sleep(Duration::from_secs(1)).await;
+    "Whats happening!?"
+}
+
+async fn health_handler() -> Json<Value> {
+    let version = env!("CARGO_PKG_VERSION");
+
+    Json(json!({
+        "app": "picoservice-rs",
+        "version": version,
+        "status": "ok",
+    }))
 }
